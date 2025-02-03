ba39fbce449acf43c327d78191b44addef88e63a
daviddwilliams13 <dwillshaan@gmail.com>
2025-02-03 00:29:14 -0500

    Modified InventoryController and InventoryControllerTest files.

--- starter/src/main/java/com/starter/fullstack/rest/InventoryController.java
+++ starter/src/main/java/com/starter/fullstack/rest/InventoryController.java
@@ -3,9 +3,7 @@ package com.starter.fullstack.rest;
 import com.starter.fullstack.api.Inventory;
 import com.starter.fullstack.dao.InventoryDAO;
 import java.util.List;
-
 import javax.validation.Valid;
-
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,6 +40,7 @@ public class InventoryController {
  @PostMapping
   public Inventory saveInventory(@Valid @RequestBody Inventory inventory) {
     return this.inventoryDAO.create(inventory);
-  }    
+  }  
+
 }
 
--- starter/src/test/java/com/starter/fullstack/rest/InventoryControllerTest.java
+++ starter/src/test/java/com/starter/fullstack/rest/InventoryControllerTest.java
@@ -5,8 +5,8 @@ import com.starter.fullstack.api.Inventory;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,13 +14,10 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.jar.Attributes.Name;
 
 /**
  * Inventory Controller Test.
@@ -89,4 +86,5 @@ public class InventoryControllerTest {
 
     Assert.assertEquals(2, this.mongoTemplate.findAll(Inventory.class).size());
   }
+
 }
